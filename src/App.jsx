e97a5255e1d52841b236088e4d14f5fc3c8f3f55
Pine <pinetcht@gmail.com>
2024-05-21 17:44:52 +0200

    added input field and set up add and get in firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react';
-import './App.css';
-import { db } from '../firebase';
-import { addDoc, getDoc, doc, collection } from 'firebase/firestore';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "../firebase";
+import { addDoc, getDocs, doc, collection } from "firebase/firestore";
 
 function App() {
-    
-  const [answer, setAnswer] = useState("")
-  const [question, setQuestion] = useState("")
-  const fetchDoc = async () => {
-    try {
-      const docRef = await getDoc(doc(db, "poll", "EvB5BhdItzwtkVdqiYnD"));
-      if (docRef.exists()) {
-        const question = docRef.data().question;
-        setQuestion(question);
+  const [answers, setAnswers] = useState(null);
+  const [answerList, setAnswerList] = useState([]);
 
-      } else {
-        console.log("No such document!");
+
+  useEffect(() => {
+    const fetchDoc = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "poll"));
+        if (querySnapshot != null) {
+          const allAnswers = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+          setAnswerList(allAnswers)
+
+          console.log(allAnswers)
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Cannot load answer", error);
       }
-    } catch(error) {
-      console.error("Cannot load answer", error);
-    }
-  }
-  fetchDoc();
-  
-    return (
+    };
+
+    fetchDoc();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const docRef = await addDoc(collection(db, "poll"), {
+      answer: answers,
+    });
+  };
+
+  return (
     <>
-    <h1>What's your favorite cereal?</h1>
-     
+      <h1>What's your favorite cereal?</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Answer:</label>
+        <input type="text" onChange={(e) => setAnswers(e.target.value)}></input>
+        <button type="submit"> Submit</button>
+      </form>
+
+      <h2>Vote for your fav cereal!</h2>
+      {
+        answerList.map((answer, key) => 
+          <p>{ answer }</p>
+        )
+      }
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
